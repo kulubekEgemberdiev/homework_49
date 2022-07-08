b00d84116760a81f78c669ac4fa046b5c2846333
KulubekEgemberdiev <askatovich61.@gmail.com>
2022-07-08 17:45:03 +0600

    Added update view. Added update.html. Added link to update.html in urls.py.

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from todolist.views import Index, DetailView, DeleteView
+from todolist.views import Index, DetailView, DeleteView, CreateView, UpdateView
 
 urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("/todolist/<int:pk>/detail/", DetailView.as_view(), name="detail"),
     path("/todolist/<int:pk>/delete/", DeleteView.as_view(), name="delete"),
+    path("/todolist/<int:pk>/update/", UpdateView.as_view(), name="update"),
+    path("/todolist/create/new/", CreateView.as_view(), name="create")
 ]
