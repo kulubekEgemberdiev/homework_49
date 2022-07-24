e5cf89fc19318fb1670f989d77119f66f7f05af1
KulubekEgemberdiev <askatovich61.@gmail.com>
2022-07-25 01:54:32 +0600

    Created a project model. Associated TodolistModel with a Project with a foreign key.

--- todolist/models.py
+++ todolist/models.py
@@ -1,7 +1,24 @@
+from datetime import date
+
 from django.db import models
 
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=30, verbose_name="Name")
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name="Description")
+    beginning_date = models.DateField(verbose_name="Beginning date")
+    expiration_date = models.DateField(null=True, verbose_name="Expiration date")
+
+    def __str__(self):
+        return f"{self.id}. {self.name}. {self.beginning_date}. {self.expiration_date}."
+
+    class Meta:
+        db_table = "project"
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
 
 class TodolistModel(models.Model):
     summary = models.CharField(max_length=30, verbose_name="Summary")
@@ -10,6 +27,7 @@ class TodolistModel(models.Model):
     types = models.ManyToManyField("todolist.Types", related_name="todolist", blank=True)
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
     updated_date = models.DateTimeField(auto_now=True, verbose_name="Updated date")
+    project = models.ForeignKey("todolist.Project", on_delete=models.PROTECT, verbose_name="Project")
 
     def __str__(self):
         return f"{self.id}. {self.summary}. {self.types}. {self.status}. {self.created_date}. {self.updated_date}"
