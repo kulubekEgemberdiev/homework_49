ef21e03f04ae3d0f1af49da5306cd231465d3f93
KulubekEgemberdiev <askatovich61.@gmail.com>
2022-08-16 16:33:27 +0600

    Created model 'Profile'. Added MEDIA_ROOT, MEDIA_URL.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+
 # Create your models here.
+
+
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars', null=True, blank=True, verbose_name='Avatar')
+    github = models.URLField(max_length=150, null=True, blank=True, verbose_name='Link to GitHub')
+    about_info = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Yourself info')
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='profile', verbose_name='User')
